7c9ce6e8bc3c60984c85e6f44812ca4c8e063de6
Mauri <mauri.monzon91@gmail.com>
2022-06-14 23:32:49 -0300

    prueba 2

--- js/complementario1.js
+++ js/complementario1.js
@@ -10,22 +10,28 @@ Podemos crear arrays para los objetos identificados en el simulador la clase ant
  */
 
 
-function Productos(id, nombre, precio){
-    this.id=id;
-    this.nombre=nombre;
-    this.precio=precio;
+class Producto {
+    constructor(id, nombre, precio, stock) {
+        this.id = id;
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.stock = parseInt(stock);
+    }
 }
 
 
-const listaProductos=[]
-let contador=2
+const productos = [];
 
+console.log("Programa para ingresar productos");
 
+let entrada = prompt("Cuantos productos va agregar?")
 
-do{
-    let nombreProducto=prompt("Ingrese el nombre del producto");
-    listaProductos.push(nombreProducto);
-    contador-=1
-} while (contador!=0)
-
-console.log(listaProductos);
+for (let index = 0; index < entrada; index++) {
+    let idProducto = index + 1
+    let nombreProducto = prompt("Ingrese nombre del producto")
+    let precioProducto = prompt("Ingrese precio del producto")
+    let stockProducto = prompt("Ingrese el stock")
+    productos.push(new Producto(idProducto, nombreProducto, precioProducto, stockProducto));
+}
+console.log("Detalle de los productos ingresados");
+console.log(productos);
